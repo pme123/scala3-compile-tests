d96ceb224fb3c7308de3100faf7613b2cbdc9d06
Pascal Mengelt <pascal.mengelt@finnova.com>
2023-11-25 11:30:00 +0100

    Added Test Results

--- src/main/scala/scala3/compile/tests/exports.scala
+++ src/main/scala/scala3/compile/tests/exports.scala
@@ -3,9 +3,9 @@ package scala3.compile.tests
 import com.github.plokhotnyuk.jsoniter_scala.core.JsonValueCodec
 import com.github.plokhotnyuk.jsoniter_scala.macros.{CodecMakerConfig, JsonCodecMaker}
 
-val numberOfTestFiles = 10
-val numberOfClass = 10
-val numberOfEnum = 30
+val numberOfTestFiles = 100
+val numberOfClass = 5
+val numberOfEnum = 5
 
 inline def JsonEnumCodec[A]: JsonValueCodec[A] = JsonCodecMaker.makeWithoutDiscriminator[A]
 inline def JsonCodec[A]: JsonValueCodec[A] = JsonCodecMaker.make{
